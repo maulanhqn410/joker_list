702ec838405a27d682c64496e47427314481450e
anh <maulanhqn1234@gmail.com>
2019-12-24 19:59:34 +0700

    add file

---
+++ src/component/Jokers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Jokers extends Component {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+export default Jokers
